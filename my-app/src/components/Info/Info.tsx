daebe619276d2bf2c93998b77e2505e07a5d637a
Kayla <krspecie@gmail.com>
2025-12-01 14:00:22 +0000

    fixed error - changed audio  file from import to url

--- my-app/src/components/Info/Info.tsx
+++ my-app/src/components/Info/Info.tsx
@@ -1,30 +1,4 @@
-//import React from 'react';
-//import LinkedInIcon from '../assets/LinkedInIcon.png';
 import styles from './Info.module.css';
-import KaylaSpecieSummary from '../../../public/KaylaSpecieSummary.m4a'
-
-// interface CardProps {
-//     icon?: string | React.ReactNode;
-//     title: string;
-//     content?: string;
-// }
-
-
-// function Card({ icon, title, content }: CardProps){
-//     return (
-//         <div className={styles.card}>
-//             <h2 className={styles.cardTitle}>
-//                 {icon && (
-//                     typeof icon === 'string' ? (
-//                     <span className={styles.cardIcon}>{icon}</span>
-//                     ) : (icon)
-//                 )}
-//                 {title}
-//             </h2>
-//             <p className={styles.cardContent}>{content}</p>
-//         </div>
-//     )
-// }
 
 export default function Info(){
     return (
@@ -33,25 +7,10 @@ export default function Info(){
           <p className={styles.funFont2}>Because <a href="https://notebooklm.google/" target="_blank" rel="noopener noreferrer">NotebookLM</a> gives a better elevator pitch than I can: </p>
           
           <audio controls>
-            <source src={KaylaSpecieSummary} type='audio/mp4' />
+            <source src="/KaylaSpecieSummary.m4a" type='audio/mp4' />
             Your browser does not support the video tag.
           </audio>
         </div>
-
-        {/* <div className={styles.cardContainer}>
-          <Card 
-            icon="📍" 
-            title="London, UK" 
-            />
-          <Card 
-            icon={<img src={LinkedInIcon} alt="LinkedIn Logo" className={styles.cardIcon} />} 
-            title="LinkedIn"  
-            />
-          <Card 
-            icon="" 
-            title="GitHub" 
-          />
-        </div> */}
       </div>
     );
 }

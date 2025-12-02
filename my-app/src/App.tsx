6ac4559324d2c9c77ef266cd74d8ea1318d4792c
Kayla <krspecie@gmail.com>
2025-12-02 17:28:06 +0000

    created project demo section.

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,4 +1,3 @@
-import EmblaCarousel from './components/EmblaCarousel/EmblaCarousel.tsx';
 import Info from './components/Info/Info.tsx';
 import styles from './App.module.css'
 import AboutSection from './components/AboutSection/AboutSection.tsx';
--- my-app/src/components/ProjectDemoSection/ProjectDemoSection.tsx
+++ my-app/src/components/ProjectDemoSection/ProjectDemoSection.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import styles from './ProjectDemoSection.module.css';
 import EmblaCarousel from '../EmblaCarousel/EmblaCarousel';
 
